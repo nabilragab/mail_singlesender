8b94256fba942ab6dbb9bd7098f186d139c0a351
Nabil Ragab <nabil@ragab.net>
2023-02-13 23:39:32 +0200

    [MIG] v14
    -bump version
    update license

--- README.md
+++
@@ -1,2 +0,0 @@
-# mail_singlesender
-Override Odoo Mail Sending and use a Single Mail Server
--- __manifest__.py
+++ __manifest__.py
@@ -1,14 +1,37 @@
 # -*- coding: utf-8 -*-
+#################################################################################
+# Author      : ROOK FZ LLC (<https://rook.ae/>)
+# Copyright(c): 2020-Present ROOK.ae
+# All Rights Reserved.
+#
+# This program is copyright property of the author mentioned above.
+# You can`t redistribute it and/or modify it.
+#
+# You should have received a copy of the License along with this program.
+# If not, see <https://rook.ae/license/>
+#################################################################################
 {
     'name': 'Mail Single Server',
+    'description': 'This module removes multiple sender functionality and makes only one active email for sender '
+                   'set using System Parameters: '
+                   'single.sender.email.rfc = mail@server.com '
+                   'single.sender.email = Company Name <mail@server.com>',
     'summary': 'Mail Single Server',
-    'sequence': '9010',
     'category': 'Mail',
-    'description': """
-    This module removes multiple sender functionality and makes only one active email for sender
-    set using System Parameters: 
-    single.sender.email.rfc = mail@server.com
-    single.sender.email = Company Name <mail@server.com>
-    """,
+    'version': '14.0.1.0.0',
+    'author': 'Nabil Mohamed Ali Ragab',
+    'company': 'ROOK FZ LLC',
+    'maintainer': 'ROOK FZ LLC',
+    'website': "https://www.rook.ae",
     'depends': ['mail', 'base', 'base_setup'],
+    'data': [],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'license': 'Other proprietary',
+    'images': ['static/description/banner.jpg'],
+    'currency': 'USD',
+    'price': 0.00,
+    'support': 'nabil@rook.ae',
+
 }
