292363ee49d4c3e7b4fd5be4a1e9ee1bc9ecf1cd
Nabil Ragab <nabil@ragab.net>
2023-03-20 19:31:59 +0200

    [UPD] Update license and manifest for company RESON

--- __manifest__.py
+++ __manifest__.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 #################################################################################
-# Author      : ROOK FZ LLC (<https://rook.ae/>)
-# Copyright(c): 2020-Present ROOK.ae
+# Author      : Reson for IT Solutions (<https://reson.ae/>)
+# Copyright(c): 2020-Present reson.ae
 # All Rights Reserved.
 #
 # This program is copyright property of the author mentioned above.
 # You can`t redistribute it and/or modify it.
 #
 # You should have received a copy of the License along with this program.
-# If not, see <https://rook.ae/license/>
+# If not, see <https://reson.ae/license/>
 #################################################################################
 {
     'name': 'Mail Single Server',
@@ -20,9 +20,9 @@
     'category': 'Mail',
     'version': '15.0.1.0.0',
     'author': 'Nabil Mohamed Ali Ragab',
-    'company': 'ROOK FZ LLC',
-    'maintainer': 'ROOK FZ LLC',
-    'website': "https://www.rook.ae",
+    'company': 'Reson for IT Solutions',
+    'maintainer': 'Reson for IT Solutions',
+    'website': "https://www.reson.ae",
     'depends': ['mail', 'base', 'base_setup'],
     'data': [],
     'installable': True,
@@ -32,6 +32,6 @@
     'images': ['static/description/banner.jpg'],
     'currency': 'USD',
     'price': 0.00,
-    'support': 'nabil@rook.ae',
+    'support': 'nabil@reson.ae',
 
 }
